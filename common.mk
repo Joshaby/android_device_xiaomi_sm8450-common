f711f02e07fca8f46783f336060434a84c0bd01b
basamaryan <basam.aryan@gmail.com>
2025-12-13 18:02:38 -0800

    sm8450-common: overlay: Reduce blur radius
    Change-Id: Id6339df82156099ca14da684f236931b3ecf51ce

--- common.mk
+++ common.mk
@@ -320,6 +320,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
     SettingsResCommon \
+    Launcher3ResCommon\
     SystemUIResCommon \
     TelephonyResCommon \
     WifiResCommon

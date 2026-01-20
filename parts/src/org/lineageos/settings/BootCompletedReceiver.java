8acf1de3f84fcd9b6f44e832d00b25a7499fe140
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2026-01-20 14:55:53 -0300

    sm8450-common: parts: Add PowerTools v3.0

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -35,6 +35,7 @@ import android.view.Display.HdrCapabilities;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.thermal.ThermalTileService;
+import org.lineageos.settings.powertools.PowerProfileTileService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -80,6 +81,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Start Thermal Management Services
         ThermalUtils.startService(context);
+
+        // Start Power Profile Tile Service
+        context.startServiceAsUser(new Intent(context, PowerProfileTileService.class), UserHandle.CURRENT);
     }
 
     private void overrideHdrTypes(Context context) {

adf9a82c5910834218fa932030ab690a81d61cd4
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2026-03-30 11:40:12 -0300

    sm8450-common: rootdir: Do not allow restricted tasks to run on big cores
    The restricted cpuset is for system tasks that are
    throttled because the screen is off. Google only
    runs these tasks on the little cluster
    to save power and we will follow suit.

--- rootdir/bin/init.kernel.post_boot-taro.sh
+++ rootdir/bin/init.kernel.post_boot-taro.sh
@@ -83,6 +83,7 @@ echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 # cpuset parameters
 echo 0-3 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-3 > /dev/cpuset/restricted/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost

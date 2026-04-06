91945cc6026f85f891cc5ff177faa213405b7936
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2026-04-06 20:38:50 -0300

    sm8450-common: thermal: Use therma-normal

--- parts/src/org/lineageos/settings/thermal/ThermalUtils.kt
+++ parts/src/org/lineageos/settings/thermal/ThermalUtils.kt
@@ -163,7 +163,7 @@ private constructor(
         private const val THERMAL_ENABLED = "thermal_enabled"
 
         private const val THERMAL_SCONFIG = "/sys/class/thermal/thermal_message/sconfig"
-        private const val THERMAL_STATE_DEFAULT = "20" // thermal-mgame.conf
+        private const val THERMAL_STATE_DEFAULT = "0" // thermal-normal.conf
 
         private val DEFAULT_VALUE = ThermalState.values().joinToString(":") { "${it.prefix}," }
 

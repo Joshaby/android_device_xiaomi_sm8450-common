a7ca943bcf2b6b9ee80d1fb99f9a39208c01d1da
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2026-04-06 16:36:07 -0300

    sm8450-common: Enable R8 optimizations for system_server and SystemUI and reduce system server verbosity

--- common.mk
+++ common.mk
@@ -552,3 +552,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
+# Enable whole-program R8 Java optimizations for SystemUI and system_server,
+# but also allow explicit overriding for testing and development.
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+
+# Reduce system server verbosity
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false

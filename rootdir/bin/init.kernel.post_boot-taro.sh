9b4cd0bbe27bf7589ee7df0b626f0686c7b0e1d5
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2026-05-04 12:00:24 -0300

    sm8450-common: post_boost: Increase WALT input boost. Tune sched upmigrate and downmigrate

--- rootdir/bin/init.kernel.post_boot-taro.sh
+++ rootdir/bin/init.kernel.post_boot-taro.sh
@@ -63,10 +63,10 @@ echo 1 > /sys/devices/system/cpu/cpu7/core_ctl/nr_prev_assist_thresh
 echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
 
 # Setting b.L scheduler parameters
-echo 95 95 > /proc/sys/walt/sched_upmigrate
-echo 85 85 > /proc/sys/walt/sched_downmigrate
-echo 100 > /proc/sys/walt/sched_group_upmigrate
-echo 85 > /proc/sys/walt/sched_group_downmigrate
+echo 85 85 > /proc/sys/walt/sched_upmigrate
+echo 65 75 > /proc/sys/walt/sched_downmigrate
+echo 95 > /proc/sys/walt/sched_group_upmigrate
+echo 75 > /proc/sys/walt/sched_group_downmigrate
 echo 1 > /proc/sys/walt/sched_walt_rotate_big_tasks
 echo 400000000 > /proc/sys/walt/sched_coloc_downmigrate_ns
 echo 39000000 39000000 39000000 39000000 39000000 39000000 39000000 5000000 > /proc/sys/walt/sched_coloc_busy_hyst_cpu_ns
@@ -109,7 +109,7 @@ if [ $rev == "1.0" ]; then
 else
 	echo 1171200 0 0 0 0 0 0 0 > /proc/sys/walt/input_boost/input_boost_freq
 fi
-echo 100 > /proc/sys/walt/input_boost/input_boost_ms
+echo 250 > /proc/sys/walt/input_boost/input_boost_ms
 
 # configure governor settings for gold cluster
 echo "walt" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
